b3504aeddbc3e3cbaa896b3231ce62def9c10321
Michael Elkins <Michael.Elkins@cobham.com>
2009-06-10 09:39:12 -0700

    configure detects the presence of system log4cxx, libsqlite3 and libconfig++, falling back to using a version installed in watcher/{include,lib} if not present
    added watcher/src/Makefile.top and watcher/src/client/Makefile.clients to put
    common settings that can be included in various Makefile.am files

---
+++ src/libwatcher/labelDisplayInfo.h
@@ -0,0 +1,64 @@
+#ifndef LABEL_DISPLAY_INFO_H
+#define LABEL_DISPLAY_INFO_H
+
+#include <string>
+#include "watcherTypes.h"
+#include "watcherColors.h"
+#include "labelMessage.h"
+#include "displayInfo.h"
+
+namespace watcher
+{
+    /** 
+     * @class LabelDisplayInfo
+     * @author Geoff.Lawler <Geoff.Lawler@cobham.com> 
+     *
+     * Keep track of display information for a Label.
+     *
+     */
+    class LabelDisplayInfo : 
+        public DisplayInfo
+    {
+        public:
+            LabelDisplayInfo();
+            virtual ~LabelDisplayInfo(); 
+
+            watcher::event::Color backgroundColor;
+            watcher::event::Color foregroundColor;
+
+            std::string fontName; // not really supported
+            float pointSize;
+
+            /** The text in the label */
+            std::string labelText; 
+
+            /** This doesn't really belong here, but I've nowhere else to put it. */
+            Timestamp expiration; 
+
+            /** Can be loaded from a LabelMessage */
+            bool loadConfiguration(const watcher::event::LabelMessagePtr &labelMessage); 
+
+            /** Can be loaded from cfg file ... */
+            bool loadConfiguration(const watcher::event::GUILayer &layer); 
+
+            /** Can be saved to cfg file ... */
+            void saveConfiguration(); 
+
+            virtual std::ostream &toStream(std::ostream &out) const;
+            std::ostream &operator<<(std::ostream &out) const { return toStream(out); }
+
+        protected:
+
+        private:
+
+            DECLARE_LOGGER();
+    };
+
+    typedef boost::shared_ptr<LabelDisplayInfo> LabelDisplayInfoPtr; 
+
+    std::ostream &operator<<(std::ostream &out, const watcher::LabelDisplayInfo &obj);
+}
+
+
+#endif // LABEL_DISPLAY_INFO_H
+
