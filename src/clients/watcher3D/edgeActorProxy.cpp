8e501866605ad926dd93f628a6c775940630728a
geoff lawler <geoff.lawler@cobham.com>
2009-12-04 09:04:40 -0500

    legacy watcher: "set gps scale" and "set all edge width" now available in GUI under the view menu. The 2 things that are always set improperly can now be fixed in GUI! yay!

---
+++ src/clients/watcher3D/edgeActorProxy.cpp
@@ -0,0 +1,87 @@
+/* Copyright 2009 SPARTA, Inc., dba Cobham Analytic Solutions
+ * 
+ * This file is part of WATCHER.
+ * 
+ *     WATCHER is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     WATCHER is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with Watcher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+// Watcher includes
+#include "logger.h"
+
+// Delta3D includes
+#include <disable_watcher_logging.h> /* undef watcher logging macros */
+#include <dtDAL/enginepropertytypes.h>
+#include <dtGame/messagetype.h>
+#include <enable_watcher_logging.h> /* redef watcher logging macros */
+
+// Watcher3D includes
+#include "edgeActorProxy.h"
+
+INIT_LOGGER(EdgeActorProxy, "EdgeActorProxy");
+
+EdgeActorProxy::EdgeActorProxy()
+{
+    TRACE_ENTER();
+    TRACE_EXIT();
+}
+
+EdgeActorProxy::~EdgeActorProxy()
+{
+    TRACE_ENTER();
+    TRACE_EXIT();
+}
+
+void EdgeActorProxy::BuildPropertyMap()
+{
+    TRACE_ENTER();
+    TransformableActorProxy::BuildPropertyMap();
+
+    EdgeActor *edgeActor = dynamic_cast<EdgeActor*> (GetActor());
+
+    // Add the "HeadPos" property
+    AddProperty(new dtDAL::Vec3ActorProperty("headPos","Head Position",
+        dtDAL::MakeFunctor(*edgeActor,&EdgeActor::SetHeadPos),
+        dtDAL::MakeFunctorRet(*edgeActor,&EdgeActor::GetHeadPos),
+        "Sets/gets the edge's head position as a 3-dimensional vector (x,y,z) from the origin.", "Position"));
+
+    // Add the "TailPos" property
+    AddProperty(new dtDAL::Vec3ActorProperty("tailPos","Tail Position",
+        dtDAL::MakeFunctor(*edgeActor,&EdgeActor::SetTailPos),
+        dtDAL::MakeFunctorRet(*edgeActor,&EdgeActor::GetTailPos),
+        "Sets/gets the edge's tail position as a 3-dimensional vector (x,y,z) from the origin.", "Position"));
+    TRACE_EXIT();
+}
+
+void EdgeActorProxy::OnEnteredWorld()
+{
+   // Register for game events
+   RegisterForMessages(dtGame::MessageType::INFO_GAME_EVENT);
+
+   // Register for ticks
+   if (IsRemote())
+      RegisterForMessages(dtGame::MessageType::TICK_REMOTE, dtGame::GameActorProxy::TICK_REMOTE_INVOKABLE);
+   else
+      RegisterForMessages(dtGame::MessageType::TICK_LOCAL, dtGame::GameActorProxy::TICK_LOCAL_INVOKABLE);
+
+   dtGame::GameActorProxy::OnEnteredWorld();
+}
+
+void EdgeActorProxy::CreateActor()
+{
+    TRACE_ENTER();
+    SetActor(*new EdgeActor(*this));
+    TRACE_EXIT();
+}
+
---
+++ src/watcherd/sqliteDatabase.cpp
@@ -0,0 +1,127 @@
+/* Copyright 2009 SPARTA, Inc., dba Cobham Analytic Solutions
+ * 
+ * This file is part of WATCHER.
+ * 
+ *     WATCHER is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     WATCHER is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with Watcher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**@file
+ * @author Michael.Elkins@cobham.com
+ * @date 2009-05-04
+ */
+
+#include <boost/scoped_ptr.hpp>
+#include <boost/shared_ptr.hpp>
+#include <sstream>
+
+//external deps
+#include "sqlite_wrapper.h"
+#include "logger.h"
+
+// class declaration
+#include "sqliteDatabase.h"
+
+#include "libwatcher/message.h"
+
+using namespace watcher;
+using namespace watcher::event;
+using namespace sqlite_wrapper;
+
+INIT_LOGGER(SqliteDatabase, "Database.SqliteDatabase");
+
+SqliteDatabase::SqliteDatabase(const std::string& path) :
+    conn_(new Connection(path, Connection::readwrite | Connection::create | Connection::nomutex))
+{
+    TRACE_ENTER();
+
+    /* Create database if it doesn't yet exist */
+    conn_->execute("CREATE TABLE IF NOT EXISTS events ( ts INTEGER, evtype INTEGER, node TEXT, data TEXT ); "
+                   "CREATE INDEX IF NOT EXISTS time ON events ( ts ASC );");
+
+    /*
+     * This must come after the db creation otherwise it will fail saying that
+     * table "events" does not exist.
+     */
+    insert_stmt_.reset(new Statement(*conn_, "INSERT INTO events VALUES (?,?,?,?)"));
+
+    TRACE_EXIT();
+}
+
+namespace {
+    /* Provide a conversion for MessageType */
+    Statement& operator<< (Statement& s, MessageType t) { return s << static_cast<int>(t); }
+}
+
+void SqliteDatabase::storeEvent(MessagePtr msg)
+{
+    TRACE_ENTER();
+
+    // serialize event
+    std::ostringstream os;
+    msg->pack(os);
+
+    //LOG_DEBUG("serialized event: " << os.str());
+
+    // bind values to prepared statement
+    *insert_stmt_ << msg->timestamp << msg->type << msg->fromNodeID.to_string() << os.str();
+
+    sqlite_wrapper::execute(*insert_stmt_);
+
+    TRACE_EXIT();
+}
+
+void SqliteDatabase::getEvents(boost::function<void(event::MessagePtr)> output,
+                               Timestamp t, Direction d, unsigned int count)
+{
+    TRACE_ENTER();
+    std::ostringstream os;
+    os << "SELECT data FROM events WHERE ts" << (d == forward ? ">" : "<") << t <<
+        " ORDER BY ts " << (d == forward ? "ASC" : "DESC") <<
+        " LIMIT " << count;
+    LOG_DEBUG(os.str());
+
+    // read each serialized event from a row, unpack and pass to callback function
+    Statement st(*conn_, os.str());
+    for (Row r(st.rows()); r; ++r) {
+        Column c(r.columns());
+        std::string data;
+        c >> data;
+        LOG_DEBUG("attempting to deserialize data from db: " << data);
+        std::istringstream is(data);
+        output(Message::unpack(is));
+    }
+    TRACE_EXIT();
+}
+
+TimeRange SqliteDatabase::eventRange()
+{
+    Timestamp begin = 0, end = 0;
+
+    TRACE_ENTER();
+
+    Statement s(*conn_,
+                "SELECT * FROM"
+                "(SELECT ts from events ORDER BY ts ASC LIMIT 1)"
+                "JOIN"
+                "(SELECT ts from events ORDER BY ts DESC LIMIT 1)");
+    Row r(s.rows());
+    Column c(r.columns());
+    c >> begin >> end;
+
+    LOG_DEBUG("begin=" << begin << " end=" << end);
+
+    TRACE_EXIT();
+
+    return TimeRange(begin, end);
+}
