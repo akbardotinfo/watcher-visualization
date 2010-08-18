6e5e28b4df2ecd6d4b3c03930da5aabf04ede1c7
geoff lawler <geoff.lawler@cobham.com>
2010-08-18 15:43:23 -0400

    remove obsolete build script for legacy watcher.

---
+++ src/clients/randomScenario/randomScenario.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+#include <libwatcher/client.h>
+#include <libwatcher/labelMessage.h>
+#include <libwatcher/edgeMessage.h>
+#include <libwatcher/gpsMessage.h>
+#include <libwatcher/colorMessage.h>
+#include <libwatcher/colors.h>
+#include <libwatcher/watcherColors.h>
+#include <libwatcher/connectivityMessage.h>
+#include <libwatcher/sendMessageHandler.h>
+
+#include <logger.h>
+
+#include "configuration.h"
+
+using namespace std;
+using namespace watcher;
+using namespace watcher::event;
+namespace po=boost::program_options;
+namespace rs=randomScenario;
+
+int main(int argc, char **argv) 
+{
+    // Load config and bail on error. 
+    if (!rs::loadConfiguration(argc, argv))
+        return EXIT_FAILURE;
+
+    rs::printConfiguration(cout); 
+
+    srand(time(NULL));
+
+    po::variables_map &config=rs::getConfig();
+    unsigned int nodeNum=config["nodeNum"].as<unsigned int>(); 
+    float nodeDegreePercentage=config["nodeDegreePercentage"].as<float>();
+    float nodeLabelPercentage=config["nodeLabelPercentage"].as<float>();
+    unsigned int layerNum=config["layerNum"].as<unsigned int>();
+    int duration=config["duration"].as<int>(); 
+    string server=config["server"].as<string>(); 
+    unsigned int maxWidth=config["width"].as<unsigned int>();
+    unsigned int maxLength=config["length"].as<unsigned int>();
+    unsigned int maxHeight=config["height"].as<unsigned int>();
+
+    LOAD_LOG_PROPS(config["logproperties"].as<string>());
+
+    cout << "Connecting to watcherd on " << server << endl;
+    watcher::Client client(server); 
+    client.addMessageHandler(MultipleMessageHandler::create());
+
+    while (duration) { 
+        GPSMessagePtr gpsMess(GPSMessagePtr(new GPSMessage)); 
+        ConnectivityMessagePtr connMess(ConnectivityMessagePtr(new ConnectivityMessage));
+
+        for (int i=1; i<=nodeNum; i++) {
+                NodeIdentifier nid=boost::asio::ip::address_v4::address_v4(i);
+                gpsMess->x=((rand()%maxWidth)/30000.0);
+                gpsMess->y=((rand()%maxLength)/30000.0);
+                gpsMess->z=(rand()%maxHeight);
+                gpsMess->fromNodeID=nid;
+                if (!client.sendMessage(gpsMess)) 
+                    cerr << "Error sending gps message #" << i << endl;
+
+                if (rand()%100 <= (unsigned int)nodeLabelPercentage) { 
+                    LabelMessagePtr lm(new LabelMessage("Label", nid)); 
+                    if (!client.sendMessage(lm)) 
+                        cerr << "Error sending node label message"; 
+                }
+
+                for (unsigned int l=0; l<layerNum; l++) {
+                    connMess->fromNodeID=nid;
+                    connMess->layer="ConnectivityMessages_" + boost::lexical_cast<string>(l);
+                    for (int n=0; n<nodeNum*(nodeDegreePercentage/100.0); n++) 
+                        connMess->neighbors.push_back(boost::asio::ip::address_v4::address_v4((rand()%nodeNum)+1));
+                    if (!client.sendMessage(connMess)) 
+                        cerr << "Error sending connectivity message #" << i << endl;
+                    connMess->neighbors.clear();
+                }
+        }
+
+        if (duration>0)
+            duration--;
+
+        sleep(1);
+    }
+
+    client.wait();
+    client.close();
+
+    return EXIT_SUCCESS;
+}
---
+++ src/libwatcher/nodeStatusMessage.cpp
@@ -0,0 +1,124 @@
+/* Copyright 2009 SPARTA, Inc., dba Cobham Analytic Solutions
+ * 
+ * This file is part of WATCHER.
+ * 
+ *     WATCHER is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     WATCHER is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with Watcher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** 
+ * @file nodeStatusMessage.cpp
+ * @author Geoff Lawler <geoff.lawler@cobham.com>
+ * @date 2009-03-23
+ */
+#include "watcherSerialize.h"
+#include "nodeStatusMessage.h"
+#include "logger.h"
+
+using namespace std;
+using namespace watcher;
+using namespace watcher::event; 
+
+INIT_LOGGER(NodeStatusMessage, "Message.NodeStatusMessage");
+
+NodeStatusMessage::NodeStatusMessage(const statusEvent &event_) : 
+    Message(NODE_STATUS_MESSAGE_TYPE, NODE_STATUS_MESSAGE_VERSION), 
+    event(event_),
+    layer(PHYSICAL_LAYER)
+{
+    TRACE_ENTER();
+    TRACE_EXIT(); 
+}
+
+NodeStatusMessage::NodeStatusMessage(const NodeStatusMessage &rhs) : 
+    Message(rhs.type, rhs.version),
+    event(rhs.event),
+    layer(rhs.layer)
+{
+    TRACE_ENTER();
+    TRACE_EXIT(); 
+}
+
+NodeStatusMessage::~NodeStatusMessage()
+{
+    TRACE_ENTER();
+    TRACE_EXIT();
+}
+
+bool NodeStatusMessage::operator==(const NodeStatusMessage &other) const
+{
+    TRACE_ENTER();
+    bool retVal = 
+        Message::operator==(other) && 
+        event==other.event && 
+        layer==other.layer; 
+    TRACE_EXIT_RET(retVal);
+    return retVal;
+}
+
+NodeStatusMessage &NodeStatusMessage::operator=(const NodeStatusMessage &other)
+{
+    TRACE_ENTER();
+    Message::operator=(other);
+    event=other.event;
+    layer=other.layer; 
+    TRACE_EXIT();
+    return *this;
+}
+
+// virtual 
+std::ostream &NodeStatusMessage::toStream(std::ostream &out) const
+{
+    TRACE_ENTER();
+    Message::toStream(out); 
+    out << "event: " << statusEventToString(event) << " layer: " << layer; 
+    TRACE_EXIT();
+    return out;
+}
+
+std::ostream &NodeStatusMessage::operator<<(std::ostream &out) const
+{
+    TRACE_ENTER();
+    TRACE_EXIT();
+    return toStream(out); 
+}
+
+
+template <typename Archive>
+void NodeStatusMessage::serialize(Archive& ar, const unsigned int /* version */)
+{
+    TRACE_ENTER();
+    ar & boost::serialization::base_object<Message>(*this);
+    ar & event;
+    ar & layer; 
+    TRACE_EXIT();
+}
+
+// static
+string NodeStatusMessage::statusEventToString(const NodeStatusMessage::statusEvent &e)
+{
+    switch(e)
+    {
+        case connect: return "connect"; break;
+        case disconnect: return "disconnect"; break;
+    }
+    return ""; 
+}
+
+ostream &operator<<(ostream &out, const NodeStatusMessage &mess)
+{
+    mess.operator<<(out);
+    return out;
+}
+
+BOOST_CLASS_EXPORT(watcher::event::NodeStatusMessage);
