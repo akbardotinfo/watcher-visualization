eec1617b5b8e4d1e4b69788c7dcfd7c968c130f1
Michael Elkins <Michael.Elkins@cobham.com>
2010-08-18 17:32:45 -0700

    remove LIMIT from SQL query and make the "count" argument a soft limit so that all events in the same millisecond can be returned with no arbitrary limit.

---
+++ src/libwatcher/connection.h
@@ -0,0 +1,90 @@
+/* Copyright 2009 SPARTA, Inc., dba Cobham Analytic Solutions
+ * 
+ * This file is part of WATCHER.
+ * 
+ *     WATCHER is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     WATCHER is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with Watcher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file connection.h
+ * @author Geoff Lawler <geoff.lawler@cobham.com> 
+ * @date 2009-07-15
+ */
+#ifndef WATHER_CONNECTION_H
+#define WATHER_CONNECTION_H
+
+#include <boost/asio.hpp>
+#include <boost/noncopyable.hpp>
+#include <list>
+
+#include "messageHandlerFwd.h"
+#include "connection_fwd.h"
+
+namespace watcher
+{
+    /// Base class for common functionality among different types of connections to the watcher daemon.
+    class Connection :
+        private boost::noncopyable
+    {
+        public:
+            /** Create a new connection.
+             * @param io_service the ASIO service for the application
+             */
+            Connection(boost::asio::io_service& io_service);
+            virtual ~Connection();
+
+            /// convenience typedef to refer to the socket type for the connection
+            typedef boost::asio::ip::tcp::socket ConnectionSocket;
+
+            /** Retrieve the ASIO socket object associated with this connection.
+             * @return reference to socket object
+             */
+            ConnectionSocket &getSocket();
+
+            /**
+             * Set a messageHandler if you want direct access to the 
+             * responses sent via sendMessage().
+             * @param messageHandler an instance of a message handler class. 
+             */
+            void addMessageHandler(MessageHandlerPtr messageHandler); 
+
+            /**
+             * Removes a message handler set via the addMessageHandler member function.
+             * @param messageHandler an instance of a message handler class. 
+             */
+            void removeMessageHandler(MessageHandlerPtr messageHandler); 
+
+            /**
+             * Returns the address of this connection's peer.
+             */
+            const boost::asio::ip::tcp::endpoint remoteEndpoint() const { return theSocket.remote_endpoint(); }
+
+        protected:
+
+            typedef std::list<MessageHandlerPtr> MessageHandlerList;
+            MessageHandlerList messageHandlers; 
+
+            ConnectionSocket theSocket;
+
+            std::string endpoint_addr_; //< IP address of the connection endpoint
+            unsigned short endpoint_port_; //< TCP/IP port of the connection endpoint
+
+        private:
+
+            DECLARE_LOGGER(); 
+    };
+
+}
+
+
+#endif // WATHER_CONNECTION_H
---
+++ src/libwatcher/displayInfo.h
@@ -0,0 +1,112 @@
+/* Copyright 2009 SPARTA, Inc., dba Cobham Analytic Solutions
+ * 
+ * This file is part of WATCHER.
+ * 
+ *     WATCHER is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     WATCHER is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with Watcher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file displayInfo.h
+ * @author Geoff Lawler <geoff.lawler@cobham.com> 
+ * @date 2009-07-15
+ */
+#ifndef DISPLAY_INFO_H
+#define DISPLAY_INFO_H
+
+#include <boost/utility.hpp>                // for noncopyable
+#include "messageTypesAndVersions.h"        // for GUILayer
+#include "declareLogger.h"
+
+namespace watcher
+{
+    /** 
+     * Base class for keeping track of display information. Derived classes
+     * will be read in their own display configuration and use it to keep 
+     * track of the state of the display of the object. 
+     * @author Geoff Lawler <Geoff.Lawler@cobham.com> 
+     */
+    class DisplayInfo 
+    {
+        public:
+
+            /** 
+             * Creates default settings in the constructor. 
+             * @param category - the category or name of the resource in this layer. 
+             */
+            DisplayInfo(const std::string &category); 
+
+            virtual ~DisplayInfo(); 
+
+            DisplayInfo(const DisplayInfo &);
+            DisplayInfo &operator=(const DisplayInfo &);
+
+            /** 
+             * Make sure the lower level of the cfg structure exists 
+             * and pass back a path to it. Uses layerName and current 
+             * value in categoryName to build the path. 
+             * @param[in] layerName the target layer name
+             */
+            const std::string getBasePath(const watcher::event::GUILayer &layerName) const;
+
+            /**
+             * Given a layer, load the configuration found there. Search is by 
+             * member variable categoryName, so that must be set. 
+             *
+             * @param layer data of the categoryName to load. 
+             * @retval  true if config loaded
+             * @retval false otherwise
+             */
+            virtual bool loadConfiguration(const watcher::event::GUILayer &layer) = 0; 
+
+            /**
+             * This saves the current state of the display information to the 
+             * singleton config instance. If you want the changes saved for this layer  
+             * when the cfg file is written out, make sure to call this prior to doing so. 
+             * The cfg info is saved to the layer specified in the loadConfiguration() call 
+             * or the default layer, if loadConfiguration is never called. 
+             */
+            virtual void saveConfiguration() const = 0; 
+
+            /** The layer that this info is displayed on. */
+            watcher::event::GUILayer layer; 
+
+            /** 
+             * returns true if this layer exists in the configuration.
+             */
+            bool configurationExisted() const { return layerExisted; }
+
+        protected:
+
+            /** The 2nd level of the cfg path, usually "node" or "edge" */
+            std::string categoryName;
+
+            /** The "lowest" level in the cfg file */
+            const std::string baseName; 
+
+            /** The string that separates levels in teh cfg path */
+            const std::string separator; 
+
+            mutable bool layerExisted;
+
+        private:
+
+            DECLARE_LOGGER();
+
+            /** The "layer" level string */
+            const std::string layerCfgId;
+
+    };
+}
+
+#endif // DISPLAY_INFO_H
